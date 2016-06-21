2775cdd15b51879ccf3a7eb24c47d53508d66702
AkshayVGadhave <akshay@vibeosys.com>
2016-06-21 12:42:16 +0530

    One Signal Basic Integration with app
    One Signal Basic Integration with app

---
+++ FTCMobileApp/app/src/main/java/com/vibeosys/tradenow/data/adapterdata/TradeDataDTO.java
@@ -0,0 +1,12 @@
+package com.vibeosys.tradenow.data.adapterdata;
+
+/**
+ * Created by akshay on 21-06-2016.
+ */
+public class TradeDataDTO {
+
+    private double mPrice;
+    private double mSl;
+    private double mTp;
+
+}
